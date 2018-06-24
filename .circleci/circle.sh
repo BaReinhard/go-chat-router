d8817d085cfd3c96ee7316af7d71dfee8d4c5535
BaReinhard <brett@reinhards.us>
2018-06-24 14:53:59 -0700

    Updated yml configuration to use circl script, todo add gcloud configuration

--- .circleci/circle.sh
+++ .circleci/circle.sh
@@ -1,8 +1,19 @@
 
 
-# If circle branch == master
-    # deploy
-# elif CIRCLE_BRANCH == staging
-    # deploy to staging
-# else
-    # nothing
+if [ "$CIRCLE_BRANCH" = "master" ];
+then
+    PROJECT='heph-dev-core'
+elif [ "$CIRCLE_BRANCH" = "deployment" ];
+    PROJECT='uplifted-elixir-203119'
+else
+    echo "Completed"
+    exit 0
+fi
+echo "Implementing configurations for CI/D"
+exit 0
+
+
+gcloud app deploy \
+--project $PROJECT \
+
+    
